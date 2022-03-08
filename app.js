972a4aa004fd8ba8a172a6f6ce7e6c491611ffc4
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2022-03-08 15:40:37 +0100

    script

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const previousOper = document.querySelector('.previous-operand');
-const currentOper = document.querySelector('.current-operand');
-
-console.log(previousOper);
+const numberBtns = document.querySelectorAll("[data-number]");
+const operationBtns =document.querySelectorAll("[data-operation]");
+const equalsBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const allClearBtn = document.querySelector('[data-all-clear]');
+const previousOperandTextElementBtn = document.querySelector('[data-previous-operand]');
+const nextOperandTextElementBtn = document.querySelector('[data-next-operand]');
